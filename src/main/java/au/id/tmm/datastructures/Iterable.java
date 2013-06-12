39b76c90ef351f60da495c71aa7835b74f12076f
Timothy McCarthy <ebh042@gmail.com>
2013-06-12 13:44:32 +1000

    Add rough documentation

--- src/main/java/au/id/tmm/datastructures/Iterable.java
+++ src/main/java/au/id/tmm/datastructures/Iterable.java
@@ -1,5 +1,9 @@
 package au.id.tmm.datastructures;
 
+/**
+ * Interface defining the mechanism for retrieving an {@link Iterator} from an
+ * {@link Iterable} class.
+ */
 public interface Iterable<E> {
     public Iterator<E> iterator();
 }
--- src/main/java/au/id/tmm/datastructures/Iterator.java
+++ src/main/java/au/id/tmm/datastructures/Iterator.java
@@ -1,6 +1,17 @@
 package au.id.tmm.datastructures;
 
+/**
+ * Interface defining a simple Iterator.
+ */
 public interface Iterator <E> {
+
+    /**
+     * Test if the Iterator has another element.
+     */
     public boolean hasNext();
+
+    /**
+     * Return the next element in the sequence.
+     */
     public E next();
 }
