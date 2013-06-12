4e732987613a0f424ab069ca9ca631853a118c76
Timothy McCarthy <ebh042@gmail.com>
2013-06-12 13:43:44 +1000

    Add methods from Collection interface.

--- src/main/java/au/id/tmm/datastructures/list/List.java
+++ src/main/java/au/id/tmm/datastructures/list/List.java
@@ -1,20 +1,69 @@
 package au.id.tmm.datastructures.list;
 
-public interface List <E> extends au.id.tmm.datastructures.Iterable {
+import au.id.tmm.datastructures.Collection;
 
-    public void add(E toAdd);
+/**
+ * Interface defining the methods available for Lists within this data
+ * structures project. Lists are ordered {@link Collection}s that allow
+ * duplicates and are generally ordered in insertion order.
+ */
+public interface List <E> extends Collection<E> {
+
+    /**
+     * Add an element to the end of the List.
+     */
+    @Override
+    public boolean add(E toAdd);
+
+    /**
+     * Insert an element into the given index in the List.
+     */
     public void add(int index, E toAdd);
 
+    /**
+     * Remove the given element from the List.
+     * @return true if the element was successfully removed, false otherwise
+     *         (e.g. if the element was not in the List)
+     */
+    @Override
+    public boolean remove(E toRemove);
+
+    /**
+     * Remove the element at the given index.
+     */
     public void remove(int index);
 
+    /**
+     * Retrieve the element at the given index.
+     */
     public E get(int index);
 
+    /**
+     * Set the element at the given index.
+     */
     public void set(int index, E toSet);
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void clear();
 
-    public int size();
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public boolean equals(Object other);
+    public boolean contains(E e);
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isEmpty();
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getSize();
 }
